372bac7a8d53839cb3b15f102ae2e68d99ec99a0
Bashir Ahmad Ehsas <basher.ahsas@gmail.com>
2024-01-27 12:16:34 +0430

    update:

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -11,30 +11,30 @@ export const connectToDatabase = async () => {
 
     if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    // cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    //     dbName: 'saraf',
-    //     bufferCommands: false,
-    // })
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+        dbName: 'saraf',
+        bufferCommands: false,
+    })
 
-    // cached.conn = await cached.promise;
-
-    // return cached.conn;
-
-    if (!cached.promise) {
-        const opts = {
-            dbName: 'saraf',
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            bufferCommands: false,
-            bufferMaxEntries: 0,
-            useFindAndModify: false,
-            useCreateIndex: true,
-        };
-
-        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-            return mongoose;
-        });
-    }
     cached.conn = await cached.promise;
+
     return cached.conn;
+
+    // if (!cached.promise) {
+    //     const opts = {
+    //         dbName: 'saraf',
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true,
+    //         bufferCommands: false,
+    //         bufferMaxEntries: 0,
+    //         useFindAndModify: false,
+    //         useCreateIndex: true,
+    //     };
+
+    //     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    //         return mongoose;
+    //     });
+    // }
+    // cached.conn = await cached.promise;
+    // return cached.conn;
 }
