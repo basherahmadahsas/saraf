cfafa98fca5d7a446453394a27feaf43b2ad4051
Bashir Ahmad Ehsas <basher.ahsas@gmail.com>
2024-01-28 22:44:25 +0430

    update:

--- lib/validator.ts
+++ lib/validator.ts
@@ -7,7 +7,7 @@ export const eventFormSchema = z.object({
     imageUrl: z.string(),
     startDateTime: z.date(),
     endDateTime: z.date(),
-    categoryId: z.any(),
+    categoryId: z.string(),
     price: z.string(),
     isFree: z.boolean(),
     url: z.string().url()
