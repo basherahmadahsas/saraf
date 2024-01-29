bb80bcb398e9be9a1eb6cd492aaae46e0e9504d6
Bashir Ahmad Ehsas <basher.ahsas@gmail.com>
2024-01-30 00:03:47 +0430

    deploymeny

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,18 +1,31 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
-const ProfilePage = async () => {
+const ProfilePage = async ({searchParams}: SearchParamProps) => {
 
     const { sessionClaims } = auth();
     const userId = sessionClaims?.userId as string;
 
+    const ordersPage = Number(searchParams?.ordersPage) || 1;
+    const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+    const orders = await getOrdersByUser({
+        userId: userId,
+        page: ordersPage,
+    });
+
+    const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+
     const organizerEvents = await getEventsByUser({
         userId: userId,
-        page: 1,
+        page: eventsPage,
     });
 
     return (
@@ -29,18 +42,18 @@ const ProfilePage = async () => {
                 </div>
             </section>
 
-            {/* <section className='wrapper my-8'>
+            <section className='wrapper my-8'>
                 <Collection
-                    data={events?.data}
+                    data={orderedEvents}
                     emptyTitle="No event tickets purchased yet."
                     emptyStateSubtext="No worries, you can always find more events to attend."
                     collectionType="My_Tickets"
                     limit={3}
-                    page={1}
+                    page={ordersPage}
                     urlParamName='ordersPage'
-                    totalPages={2}
+                    totalPages={orders?.totalPages}
                 />
-            </section> */}
+            </section>
             {/* Events Organized */}
             <section className='bg-primary-50 bg-dotted-pattern bg-cover b-center py-5 md:py-10'>
                 <div className='wrapper flex items-center justify-center sm:justify-between'>
@@ -59,9 +72,9 @@ const ProfilePage = async () => {
                     emptyStateSubtext="Go Create some now"
                     collectionType="Events_Organized"
                     limit={6}
-                    page={1}
+                    page={eventsPage}
                     urlParamName='eventsPage'
-                    totalPages={2}
+                    totalPages={orderedEvents?.totalPages}
                 />
             </section>
         </>
--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
-
+ 
 export default authMiddleware({
-    publicRoutes: [
-        '/',
-        '/events/:id',
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-    ],
-    ignoredRoutes: [
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-    ]
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+    "/api/webhooks(.*)"
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
 });
-
+ 
 export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
